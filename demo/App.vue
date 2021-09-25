45b487e54c233809a7449889f94399c3681b9764
Collin Henderson <collinpatrickhenderson@gmail.com>
2021-09-25 01:22:08 -0400

    Watches window resize event

--- demo/App.vue
+++ demo/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="px-8 my-32 md:px-0">
+  <div class="px-8 mt-16 mb-6 sm:mt-32 sm:mb-6 md:px-0">
     <div class="flex items-center justify-between w-full mx-auto mt-8 max-w-prose">
-      <h1 class="flex items-center text-4xl font-bold">
-        <Logo class="text-purple-500 w-auto h-8 fill-current relative top-0.5" aria-hidden="true" />
-        <span class="inline-block ml-4 text-gray-600">vue-input-autowidth</span>
+      <h1 class="flex items-center text-lg font-bold sm:text-4xl">
+        <Logo class="text-purple-500 w-auto h-6 sm:h-8 fill-current relative top-0.5" aria-hidden="true" />
+        <span class="inline-block ml-2 text-gray-600 sm:ml-4">vue-input-autowidth</span>
       </h1>
-      <nav class="flex items-center">
+      <nav class="absolute top-[20px] right-[20px] flex items-center sm:relative sm:top-0 sm:right-0">
         <a
           href="https://github.com/syropian/vue-input-autowidth"
           target="_blank"
@@ -18,7 +18,13 @@
       </nav>
     </div>
     <div class="w-full mx-auto mt-8 max-w-prose">
-      <input v-model="msg" v-autowidth type="text" class="input" placeholder="Watch me change size with my content!" />
+      <input
+        v-model="msg"
+        v-autowidth
+        type="text"
+        class="block max-w-full input"
+        placeholder="Watch me change size with my content!"
+      />
     </div>
     <div class="w-full mx-auto mt-8 prose max-w-prose">
       <h2>Install</h2>
@@ -35,50 +41,41 @@
       <h3>Configure</h3>
       <p>You can pass various options to the directive</p>
       <highlightjs language="xml" :code="showOptionsCode" />
-      <input
-        v-model="msg"
-        v-autowidth="{
-          minWidth: '75px',
-          maxWidth: '75%',
-          comfortZone: '1ch',
-        }"
-        type="text"
-        class="input"
-        placeholder="An example with custom options"
-      />
-
-      <footer>
-        <p class="text-gray-500">&copy; {{ new Date().getFullYear() }} Collin Henderson</p>
-      </footer>
     </div>
+    <footer class="w-full pt-4 mx-auto mt-8 border-t border-gray-200 sm:mt-12 max-w-prose">
+      <p class="text-gray-500">
+        &copy; {{ new Date().getFullYear() }}
+        <a
+          href="https://syropia.net"
+          target="blank"
+          rel="noopener noreferrer"
+          class="font-semibold text-purple-500 hover:underline"
+          >Collin Henderson</a
+        >
+      </p>
+    </footer>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from 'vue'
+<script lang="ts" setup>
+import { ref } from 'vue'
 import Logo from './Logo.vue'
 import GitHubIcon from './GitHubIcon.vue'
 
-export default defineComponent({
-  components: {
-    Logo,
-    GitHubIcon,
-  },
-  setup() {
-    const msg = ref('')
+const msg = ref('')
 
-    const installCode = `$ npm install vue-input-autowidth@next --save
+const installCode = `$ npm install vue-input-autowidth@next --save
 # or...
 $ yarn add vue-input-autowidth@next`
 
-    const addGlobalCode = `import { createApp } from 'vue'
+const addGlobalCode = `import { createApp } from 'vue'
 import App from './App.vue'
 import { plugin as VueInputAutowidth } from 'vue-input-autowidth'
 
 createApp(App).use(VueInputAutowidth).mount("#app")
 `
 
-    const perComponentCode = `import { directive as VueInputAutowidth } from 'vue-input-autowidth'
+const perComponentCode = `import { directive as VueInputAutowidth } from 'vue-input-autowidth'
 
 export default {
   directives: { autowidth: VueInputAutowidth },
@@ -87,33 +84,23 @@ export default {
   },
 }`
 
-    const useDirectiveCode = `<input
+const useDirectiveCode = `<input
   type="text"
   placeholder="Watch me change size with my content!"
   v-model="msg"
   v-autowidth
 />`
 
-    const showOptionsCode = `<input
+const showOptionsCode = `<input
   type="text"
   placeholder="An example with custom options"
   v-model="msg"
   v-autowidth="{
     minWidth: '75px',
-    maxWidth: '75%',
+    maxWidth: '86%',
     comfortZone: '1ch',
   }"
 />`
-    return {
-      msg,
-      installCode,
-      addGlobalCode,
-      perComponentCode,
-      useDirectiveCode,
-      showOptionsCode,
-    }
-  },
-})
 </script>
 
 <style>
@@ -123,6 +110,6 @@ export default {
 }
 
 .input {
-  @apply transition-colors min-w-0 px-3 py-2 rounded-md text-lg focus:outline-none border-2 border-gray-300 focus:border-purple-500;
+  @apply transition-colors min-w-0 px-2 sm:px-3 py-1 sm:py-2 rounded-md text-base sm:text-lg focus:outline-none border-2 border-gray-300 focus:border-purple-500;
 }
 </style>
--- lib/directive.ts
+++ lib/directive.ts
@@ -4,27 +4,53 @@ type Complete<T> = {
   [P in keyof Required<T>]: Pick<T, P> extends Required<Pick<T, P>> ? T[P] : T[P] | undefined
 }
 
-interface InputWithMirror extends HTMLInputElement {
+interface AutowidthInput extends HTMLInputElement {
   mirror: HTMLElement
+  options: Complete<InputAutoWidthOptions>
+  windowResizeHandler?: () => void
+  sizerFunc?: (e?: Event) => () => void
 }
 
 export interface InputAutoWidthOptions {
   maxWidth?: string
   minWidth?: string
   comfortZone?: string
+  watchWindowSize?: boolean
+  windowResizeHandlerDebounceTime?: number
+  disableNonInputWarning?: boolean
 }
 
 const defaults: Complete<InputAutoWidthOptions> = {
-  maxWidth: 'none',
-  minWidth: 'none',
+  maxWidth: undefined,
+  minWidth: undefined,
   comfortZone: '0px',
+  watchWindowSize: true,
+  windowResizeHandlerDebounceTime: 150,
+  disableNonInputWarning: false,
 } as const
 
-const checkWidth = (el: InputWithMirror, options: Complete<InputAutoWidthOptions>) => {
-  const mirror: HTMLElement = el.mirror
+export const debounce = <T extends (...args: any[]) => any>(callback: T, waitFor: number) => {
+  let timeout: ReturnType<typeof setTimeout>
+  return (...args: Parameters<T>): ReturnType<T> => {
+    let result: any
+    timeout && clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      result = callback(...args)
+    }, waitFor)
+    return result
+  }
+}
 
-  el.style.maxWidth = options.maxWidth || 'none'
-  el.style.minWidth = options.minWidth || 'none'
+const checkWidth = (el: AutowidthInput) => {
+  const { mirror, options } = el
+
+  if (options.maxWidth) {
+    el.style.maxWidth = options.maxWidth
+  }
+
+  if (options.minWidth) {
+    el.style.minWidth = options.minWidth
+  }
 
   let val = el.value
 
@@ -45,54 +71,81 @@ const checkWidth = (el: InputWithMirror, options: Complete<InputAutoWidthOptions
   }
 }
 
-const mergeDefaultWithOptions = (options: InputAutoWidthOptions) => Object.assign({}, defaults, options)
+const copyStylesToMirror = (el: AutowidthInput) => {
+  const styles = window.getComputedStyle(el)
+  const { options } = el
+
+  Object.assign(el.mirror.style, {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    visibility: 'hidden',
+    height: '0',
+    overflow: 'hidden',
+    whiteSpace: 'pre',
+    fontSize: styles.fontSize,
+    fontFamily: styles.fontFamily,
+    fontWeight: styles.fontWeight,
+    fontStyle: styles.fontStyle,
+    letterSpacing: styles.letterSpacing,
+    textTransform: styles.textTransform,
+    paddingRight: `calc(${options.comfortZone} + ${styles.paddingRight} + ${styles.borderRightWidth})`,
+    paddingLeft: `calc(${styles.paddingLeft} + ${styles.borderLeftWidth})`,
+  })
+}
+
+const copyStylesAndCheckWidth = (el: AutowidthInput) => {
+  copyStylesToMirror(el)
+  checkWidth(el)
+}
+
+const mergeDefaultsWithOptions = (options: InputAutoWidthOptions): Complete<InputAutoWidthOptions> =>
+  Object.assign({}, defaults, options)
 
 export default {
-  beforeMount: function (el: HTMLElement) {
-    if (el.tagName.toLocaleUpperCase() !== 'INPUT') {
+  beforeMount: function (el: AutowidthInput, binding: DirectiveBinding) {
+    el.options = mergeDefaultsWithOptions(binding.value as InputAutoWidthOptions)
+
+    if (!el.options.disableNonInputWarning && el.tagName.toLocaleUpperCase() !== 'INPUT') {
       throw new Error('v-input-autowidth can only be used on input elements.')
     }
   },
-  mounted: function (el: InputWithMirror, binding: DirectiveBinding, vnode: VNode) {
-    const options: Complete<InputAutoWidthOptions> = mergeDefaultWithOptions(binding.value)
+  mounted: function (el: AutowidthInput, binding: DirectiveBinding, vnode: VNode) {
     const hasVModel = Object.prototype.hasOwnProperty.call(vnode.props, '@onUpdate:modelValue')
-    const styles = window.getComputedStyle(el)
+
+    el.sizerFunc = (_e?: Event) => checkWidth.bind(null, el)
 
     el.mirror = document.createElement('div')
-
-    Object.assign(el.mirror.style, {
-      position: 'absolute',
-      top: '0',
-      left: '0',
-      visibility: 'hidden',
-      height: '0',
-      overflow: 'hidden',
-      whiteSpace: 'pre',
-      fontSize: styles.fontSize,
-      fontFamily: styles.fontFamily,
-      fontWeight: styles.fontWeight,
-      fontStyle: styles.fontStyle,
-      letterSpacing: styles.letterSpacing,
-      textTransform: styles.textTransform,
-      paddingRight: `calc(${options.comfortZone} + ${styles.paddingRight} + ${styles.borderRightWidth})`,
-      paddingLeft: `calc(${styles.paddingLeft} + ${styles.borderLeftWidth})`,
-    })
-
+    copyStylesToMirror(el)
     el.mirror.setAttribute('aria-hidden', 'true')
-
     document.body.appendChild(el.mirror)
 
-    checkWidth(el, options)
+    copyStylesAndCheckWidth(el)
 
     if (!hasVModel) {
-      el.addEventListener('input', checkWidth.bind(null, el, options))
+      el.addEventListener('input', el.sizerFunc)
+    }
+
+    if (el.options.watchWindowSize && el.options.windowResizeHandlerDebounceTime !== undefined) {
+      const windowResizeHandler = (_e?: Event) => copyStylesAndCheckWidth(el)
+      el.windowResizeHandler = debounce(windowResizeHandler, el.options.windowResizeHandlerDebounceTime)
+      window.addEventListener('resize', el.windowResizeHandler, { passive: true })
     }
   },
-  updated: function (el: InputWithMirror, binding: DirectiveBinding) {
-    checkWidth(el, mergeDefaultWithOptions(binding.value))
+  updated: function (el: AutowidthInput) {
+    if (el.sizerFunc) {
+      el.sizerFunc()()
+    }
   },
-  unmounted: function (el: InputWithMirror, binding: DirectiveBinding) {
+  unmounted: function (el: AutowidthInput) {
     document.body.removeChild(el.mirror)
-    el.removeEventListener('input', checkWidth.bind(null, el, mergeDefaultWithOptions(binding.value)))
+
+    if (el.sizerFunc) {
+      el.removeEventListener('input', el.sizerFunc)
+    }
+
+    if (el.options.watchWindowSize && el.windowResizeHandler) {
+      window.removeEventListener('resize', el.windowResizeHandler)
+    }
   },
 } as ObjectDirective
